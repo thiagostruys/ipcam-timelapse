edee494d325febad693423d34138f689d222a3ab
thiagostruys <53374374+thiagostruys@users.noreply.github.com>
2022-02-17 10:37:48 +0100

    Update take_snapshot.py

--- take_snapshot.py
+++ take_snapshot.py
@@ -23,7 +23,25 @@ while True:
     # Capture frame from camera stream
     cap = cv2.VideoCapture(stream_url)
     ret, frame = cap.read()
+    
+    #Put conditions from weewx onto image
 
+    datafile = open(filename)
+    conditions = datafile.read()
+
+
+    position = (10,1750)
+    cv2.putText(
+        frame, #numpy array on which text is written
+        (conditions), #text
+        position, #position at which writing has to start
+        cv2.FONT_HERSHEY_COMPLEX, #font family
+        1.5, #font size
+        (255,255,255), #font color
+        3) #font stroke
+
+    datafile.close()
+    
     # Save frame as image
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
